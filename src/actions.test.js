42324c347a7e1f4d94aad9337a91e4a6b71c5642
Bailey Bryant <bailey.bryant10@gmail.com>
2017-10-02 14:15:09 -0500

    add tests for action creators

--- src/actions.test.js
+++ src/actions.test.js
@@ -1,29 +1,15 @@
-import reducer from "./reducer";
 import { createTodo, toggleTodo, filterTodos} from "./actions";
+import {CREATE_TODO, TOGGLE_TODO, FILTER_TODOS} from "./actions";
 
 
 describe("CREATE_TODO", function () {
-    test("creates a new todo", function () {
-        const initialState = {todos: [], nextId: 1, filter: 'all'}
-        const state = reducer(initialState, createTodo("Test"));
-        expect(state.todos).toHaveLength(1);
-        expect(state.todos[0]).toEqual({id: 1, done: false, text: "Test"});
+    test("creates a new todo - action", function () {
+        expect(createTodo("TEST")).toEqual({type:CREATE_TODO, payload:"TEST"})
     })
-
-
-    test("TOGGLE_TODO", function () {
-        const initialState = {todos: [], nextId: 1, filter: 'all'}
-        let state = reducer(initialState, createTodo("Test"));
-        state=reducer(state, toggleTodo(1))
-        expect(state.todos[0].done).toEqual(true);
+    test("toggle - action", function () {
+        expect(toggleTodo(1)).toEqual({type:TOGGLE_TODO, payload:1})
     })
-
-  test("FILTER_TODOS", function () {
-      const initialState = {todos: [], nextId: 1, filter: 'all'}
-      let state = reducer(initialState, createTodo("Test"));
-      state=reducer(state, toggleTodo(1))
-      state=reducer(state, filterTodos('completed'))
-      expect(state.todos).toHaveLength(1);
+    test("filter - action", function () {
+        expect(filterTodos("completed")).toEqual({type:FILTER_TODOS, payload:"completed"})
+    })
   })
-
-})
